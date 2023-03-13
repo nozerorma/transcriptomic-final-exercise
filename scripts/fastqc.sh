08acea2caf399d54bc96f03d98a882dc8fb823e7
nozerorma <n0zer0rma@gmail.com>
2023-03-13 02:34:55 +0100

    self note to beauty

--- scripts/fastqc.sh
+++ scripts/fastqc.sh
@@ -6,25 +6,28 @@
 echo -e "\nPerforming read QC...\n"
 mkdir -p out/qc/fastqc
 fastqc_dir="out/qc/fastqc"
-for sid in $(find res/samples/dumped_fastq -type f -name '*.fastq'); do
+for sid in $(find res/samples/dumped_fastq -type f -name '*.fastq' | sort -u); do
 	is_sid=$(basename "$sid" .fastq)
     base_sid=$(basename "$sid" .fastq | cut -d"_" -f1)
-	if [ -f "$fastqc_dir/$base_sid/$is_sid*.html" ]; then
-		echo -e "FastQC already performed for "$base_sid", skipping.\n"
-		continue
-	else
+	
+    if [ -f "$fastqc_dir/$base_sid/${is_sid}_fastqc.html" ]; then
+		echo -e "FastQC already performed for "$is_sid", skipping.\n"
+    
+    else
 		mkdir -p "$fastqc_dir"/"$base_sid"
 		mkdir -p "log/qc/fastqc"
 		echo "Started analysis of $sid"
 		#echo -e "\nFastQC log as of $(date +'%x                %H:%M:%S')" >> log/qc/fastqc/$base_sid.log
 		#echo -e "___________________________________________________________\n">> log/qc/fastqc/$base_sid.log 
 		fastqc -o "$fastqc_dir"/"$base_sid" "$sid" >> log/qc/fastqc/$base_sid.log
-		continue
 	fi
+
 done
+
 # Selectively show FastQC results
 read -rp "Would you like to visualize your FastQC results? This \
 is recommended for choosing the most appropriate pipeline for your workflow (Y/n): " visFastqc   
+
 case $visFastqc in
 	[Yy]* )
 		# Show fastqc reports in firefox
