ef6188b2b04a120c32860f014cfcb4b7d44eff7f
nozerorma <n0zer0rma@gmail.com>
2023-03-13 23:58:11 +0100

    added genome download if necessary

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,7 +1,18 @@
+mkdir -p res/samples	
+mkdir -p data/assembly/reference_grch38
+sample_dir="res/samples"
+genome_dir="data/assembly/reference_grch38"
+
+# Activate if required or if willing to use whole genome
+# echo -e "\Downloading GRCh38 genome, transcriptome and annotations files\n"
+# wget -nc -P $genome_dir --content-disposition "https://ftp.ensembl.org/pub/release-109/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.alt.fa.gz"
+# wget -nc -P $genome_dir --content-disposition "https://ftp.ensembl.org/pub/release-109/fasta/homo_sapiens/cdna/Homo_sapiens.GRCh38.cdna.all.fa.gz"
+# wget -nc -P $genome_dir --content-disposition "https://ftp.ensembl.org/pub/release-109/gtf/homo_sapiens/Homo_sapiens.GRCh38.109.gtf.gz"
+
 echo -e "\nEnter one or more SRA accessions (e.g. SRR123456) separated by spaces, or enter 'S' to stop: \n"
 
-sample_dir="res/samples"
 SRAentry=0
+
 
 while true; do
     	read -a SRAentries  # Read a line of input and split it into an array of accessions
