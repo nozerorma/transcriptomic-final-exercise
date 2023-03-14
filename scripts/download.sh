44ca900f4a1bb54ee284eebfe52b4d347755a320
nozerorma <n0zer0rma@gmail.com>
2023-03-14 17:11:26 +0100

    simplified dumping plus link to qc

--- scripts/download.sh
+++ scripts/download.sh
@@ -20,26 +20,59 @@ while true; do
         # If the first element is 'S', 's', 'N', 'n' or '0', exit the loop
         break
     else
+		
         for SRAentry in "${SRAentries[@]}"; do
             if [[ "$SRAentry" =~ ^[Ss]RR[0-9]{6}$ ]]; then
                 echo "Matched SRA accession $SRAentry"
                 wget -nc -P $sample_dir/SRA --content-disposition https://sra-pub-run-odp.s3.amazonaws.com/sra/$SRAentry/$SRAentry
                 echo "Finished downloading $SRAentry"
+				
+				# Dump fastq from SRA entry
+				mkdir -p $sample_dir/dumped_fastq
+				echo -e "\nDumping fastq files from $SRAentry...\n"
+					if [ "$(ls -A $sample_dir/dumped_fastq/$SRAentry)" ]; then
+						echo -e "Fastq already dumped for $SRAentry, skipping.\n"
+					else
+						fasterq-dump -fp -O $sample_dir/dumped_fastq/$SRAentry $SRAentry
+					fi				
             else
                 echo "Did not match SRA accession $SRAentry. Skipping download."
             fi
         done
+		break
     fi
 done
 
-mkdir -p $sample_dir/dumped_fastq
 
-for sra in $sample_dir/SRA/*; do
-    SRAentry=$(basename "$sra")
-    echo -e "\nDumping fastq files from $SRAentry...\n"
-    if [ "$(ls -A $sample_dir/dumped_fastq/$SRAentry)" ]; then # pass this to other similar features
-        echo -e "Fastq already dumped for $SRAentry, skipping.\n"
-    else
-        fasterq-dump -fp -O $sample_dir/dumped_fastq/$SRAentry $sample_dir/SRA/$SRAentry
-    fi
+
+
+# QC assay
+
+read -rp "Would you like to perform a QC analysis (FastQC and FastQScreen)? (Y/n): " performQC
+
+# por alguna razón (el array) hace esto, limpiar
+# basename: missing operand
+# Try 'basename --help' for more information.
+
+# Performing QC analysis...
+
+# FastQC analysis already performed for , skipping analysis.
+
+# FastQScreen analysis already performed for , skipping analysis.
+
+
+for sid in $(find $sample_dir/dumped_fastq -type f -name '*.fastq' | sort -u); do
+	base_sid=$(basename $sid | cut -d"_" -f1)
+		if [[ "${SRAentries[@]}" =~ "$base_sid" ]]; then
+			case $performQC in
+				[Yy]* )
+					bash scripts/qc.sh run $sid "out/qc/fastqc" "out/qc/fastq_screen"
+				;;
+				[Nn]* )
+					echo -e "\nSkipping QC analysis...\n" 
+				;;
+			esac
+		fi
 done
+
+bash scripts/qc.sh vis
