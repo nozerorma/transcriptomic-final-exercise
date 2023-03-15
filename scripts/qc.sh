1b0c0e8dbfab33a86dd3f0c5006499027ed5ace8
nozerorma <n0zer0rma@gmail.com>
2023-03-15 02:08:22 +0100

    cmpletely overhauled

--- scripts/qc.sh
+++ scripts/qc.sh
@@ -2,70 +2,68 @@
 
 # maybe insert logs?
 
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
 mkdir -p "out/qc/fastqc"
-mkdir -p "out/qc/fastqscreen"
+mkdir -p "out/qc/fastq_screen"
 
 sid=$2
-base_sid=$(basename $sid)
+base_sid=$(basename "$sid")
+nofastq_sid=$(basename "$sid" .fastq)
+cut_sid=$(basename "$sid" | cut -d"_" -f1)
 fastqc_dir=$3
 fastqscreen_dir=$4
 
-echo -e "\nPerforming QC analysis...\n"
-
-
 # FASTQC
-if [ "$(ls -A $fastqc_dir)" ]; then
-	echo -e "FastQC analysis already performed for $base_sid, skipping analysis.\n" 
-
-else
-	fastqc -o $fastqc_dir/$sid $sid
-
-fi
-
-# FASTQSCREEN
-if  [ "$(ls -A $fastqscreen_dir)" ]; then
-	echo -e "FastQScreen analysis already performed for $base_sid, skipping analysis.\n" 
+if [ "$1" == "run" ]; then
 	
-else
-	screen_gen="res/fastq_screen_samples/FastQ_Screen_Genomes/"
-	if [ ! "$(ls -A $screen_gen)" ]; then
-		echo "No reference genomes could be found."
-		read -rp "Would you like to download genome indexes from database? \
-		It takes long AND it takes space... (Y/n): " genDownload
-		
-		case $genDownload in
-			[Yy]* )
-				echo -e "\nDownloading genomes...\n" 
-				fastq_screen --get_genomes --outdir "res/fastq_screen_samples"
-			;;
-			[Nn]* )
-				echo -e "\nSkipping genome download...\n"
-			;;
-		esac
-	
+	echo -e "\nPerforming QC analysis for $nofastq_sid...\n"
+
+	if [ -f $fastqc_dir/$cut_sid/${nofastq_sid}_fastqc.html ]; then
+		echo -e "FastQC analysis already performed for $nofastq_sid, skipping analysis.\n" 
+
 	else
-		fastq_screen --conf "$screen_gen/fastq_screen.conf" \
-            --tag --aligner bowtie2 --subset 100000 --threads 6 \
-            --outdir "$fastqscreen_dir/$base_sid" $sid
-    fi
+		echo -e "\nRunning FastQC analysis..."
+		mkdir -p $fastqc_dir/$cut_sid
+		mkdir -p log/qc
+		fastqc -o $fastqc_dir/$cut_sid $sid 2>&1 >/dev/null | tail -n +2 2> log/qc/fastqc.log
+
+	fi
+
+	# FASTQSCREEN
+	if [ -f $fastqscreen_dir/$cut_sid/${nofastq_sid}_screen.html ]; then
+		echo -e "FastQScreen analysis already performed for $nofastq_sid, skipping analysis.\n" 
+		
+	else
+		screen_gen="res/fastq_screen_samples/FastQ_Screen_Genomes"
+		if [ ! "$(ls -A $screen_gen)" ]; then
+			echo "No reference genomes could be found."
+			echo -e "\nWould you like to download genome indexes from database?"
+			read -rp "It takes long AND it takes space... (Y/n): " genDownload
+			
+			case $genDownload in
+				[Yy]* )
+					echo -e "\nDownloading genomes...\n" 
+					fastq_screen --get_genomes --outdir "res/fastq_screen_samples"
+				;;
+				[Nn]* )
+					echo -e "\nSkipping genome download...\n"
+				;;
+			esac
+		
+		else
+			echo -e "\nRunning FastQScreen analysis...\n"
+			mkdir -p $fastqscreen_dir/$cut_sid
+			mkdir -p log/qc
+			fastq_screen --conf "$screen_gen/fastq_screen.conf" \
+				--tag --aligner bowtie2 --subset 100000 --threads 6 \
+				--outdir "$fastqscreen_dir/$cut_sid" $sid 2> log/qc/fastqscreen.log
+		fi
+	fi
 fi
 
 # Results visualization
-if [ "$1" == "vis" ]; then
-	read -rp "Visualize FastQC results in default browser (Y/n): " visFastqc
-	case $visFastqc in
-		[Yy]* )
-			# Show fastqc reports in firefox
-			echo -e "\nOpening firefox for QC report visualization...\n"
-			
-			for fastqc_rep in $(find out/qc -type f -name *.html); do
-					xdg-open $fastqc_rep >/dev/null 2>&1
-			done
-			;;
-		
-		[Nn]* )
-			# Skip fastqc visualization
-			echo -e "\nSkipping FastQC report visualization...\n"
-			;;	
-	esac
-fi
+printf "${YELLOW}\nNow, take your time to give a look to the QC analysis.\n${NC}"
+echo "When you are ready, press any key to continue..."
+read -n 1 -s -r -p ""
