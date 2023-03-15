bc30b58bdd082b11c3d01f48805649f2d41baf09
nozerorma <n0zer0rma@gmail.com>
2023-03-15 17:06:22 +0100

    reworked output and fixed samtools

--- scripts/align.sh
+++ scripts/align.sh
@@ -5,7 +5,7 @@ f_path=$2
 r_path=$3
 outdir=$4
 logdir=$5
-workflow=$6
+workflow=$14
 
 f_name=$(basename "$f_path")
 r_name=$(basename "$r_path")
@@ -13,8 +13,9 @@ base_sid=$(basename "$f_path" | cut -d"_" -f1)
 f_sid=$(basename "$f_name" .fastq)
 r_sid=$(basename "$r_name" .fastq)
 
+# Maybe change all the ls -A for -f, makes more sense
 # This section should be changed accordingly
-# It would be nice of me to glob, but I don't seem to be able
+###### It would be nice of me to glob, but I don't seem to be able
 ref_gen="data/assembly/reference_grch38/Homo_sapiens.GRCh38.dna.chromosome.21.fa"
 ref_cdna="data/assembly/reference_grch38/Homo_sapiens.GRCh38.cdna.all.fa.gz"
 ref_gtf="data/assembly/reference_grch38/Homo_sapiens.GRCh38.109.chr21.gtf"
@@ -39,39 +40,43 @@ echo -e "\nAligning $base_sid to reference with $tool...\n"
 # overview from https://www.reneshbedre.com/blog/star-aligner.html#mapping-reads-to-genome
 # STAR
 if [ "$1" == "STAR" ]; then
-	if [ "$(ls -A $outdir/$base_sid)" ]; then
-		echo -e "Alignment already performed for $base_sid, skipping alignment.\n"   
-
+	if [ "$(ls -A $outdir)" ]; then
+		echo -e "Alignment already performed for $base_sid, skipping alignment...\n"
+	
 	else
-		STAR --runThreadN 6 --readFilesIn $f_path $r_path  \
-			--genomeDir $index_dir --outReadsUnmapped Fastx  \
-			--outFileNamePrefix $outdir/$base_sid \
-			--outSAMtype BAM SortedByCoordinate 
-		
-		# add more params for statistics
-		bam_file=$(find $outdir -type f -name "*.bam")
-		samtools index $bam_file #> $logdir/$tool.log # - (necessary?)
-			# index bam here with samtools
+	
+		STAR --runThreadN 14 --readFilesIn $f_path $r_path  \
+				--genomeDir $index_dir --outReadsUnmapped Fastx  \
+				--outFileNamePrefix $outdir \
+				--outSAMtype BAM SortedByCoordinate 
+			
+			# add more params for statistics
+			bam_file=$(find $outdir -type f -name "*.bam")
+			samtools stats $bam_file > $bam_file.txt
+			samtools index $bam_file
+				# index bam here with samtools
 
-		echo -e "\nSample $base_sid aligned using $tool.\n"
-
+			echo -e "\nSample $base_sid aligned using $tool.\n"
 	fi
-
 # overview from https://bioinfo-dirty-jobs.github.io/rana2//lectures/07.rnaseq_hisat2/
 # HISAT2
 elif [ "$1" == "HISAT2" ]; then
-	if [ "$(ls -A $outdir/$base_sid)" ]; then
-		echo -e "Alignment already performed for $base_sid, skipping alignment.\n"   
-
-	else
-		hisat2 -p 6 --dta -x $index_dir -1 $f_path -2 $r_path \
-		-S $outdir/$base_sid/$base_sid.sam 
-		
-		
-		# add more params for statistics
-		samtools view -bS $outdir/$base_sid/$base_sid.sam > $base_sid.bam | \
-		samtools sort --write-index #> $logdir/$tool.log 
+	if [ "$(ls -A $outdir)" ]; then
+		echo -e "Alignment already performed for $base_sid, skipping alignment...\n"
 	
+	else
+		mkdir -p $outdir
+		hisat2 --new-summary --summary-file $outdir.hisat2.summary \
+		-p 14 -x $index_dir/HISAT2 -1 $f_path -2 $r_path -k 1 -S $outdir.sam 
+		
+		# problema con el pipe
+		# add more params for statistics
+		samtools view -bS $outdir.sam > $outdir.bam
+		samtools stats $outdir.bam > $outdir.txt
+		samtools sort --write-index $outdir.bam -o $outdir.sorted.bam
+		samtools stats $outdir.sorted.bam > $outdir.sorted.txt 
+		# was going to run picard but it has a bunch of incompatibilities
+		# with the tools I'm already using, aborting
 	fi
 
 ## ALIGNMENT
@@ -81,21 +86,24 @@ elif [ "$1" == "HISAT2" ]; then
 # SALMON (mapping-based mode, using GTF annotations)
 # -l set as A for automatic guessing of strandness, change accordingly
 elif [ "$1" == "SALMON" ]; then
-	if [ "$(ls -A $outdir/$base_sid)" ]; then
-		echo -e "Alignment already performed for $base_sid, skipping alignment.\n" 
+	if [ "$(ls -A $outdir)" ]; then
+		echo -e "Alignment already performed for $base_sid, skipping alignment...\n"
+	
 	else
 		salmon quant -i $index_dir -l A -1 $f_path -2 $r_path --validateMappings \
-			-o $outdir/$base_sid -g $ref_gtf -p 6 #> $logdir/$tool.log
+			-o $outdir -g $ref_gtf -p 14
 	fi
+
 # KALLISTO
 # tema de los gtf en ambos, no se si sin meterlo en idx tiene sentido
 elif [ "$1" == "KALLISTO" ]; then
-	if [ "$(ls -A $outdir/$base_sid)" ]; then
-		echo -e "Alignment already performed for $base_sid, skipping alignment.\n" 
+	if [ "$(ls -A $outdir)" ]; then
+		echo -e "Alignment already performed for $base_sid, skipping alignment...\n"
+	
 	else
 		base_ref_cdna=$(basename $ref_cdna .gz)
 		kallisto quant -i $index_dir/$base_ref_cdna.idx --bias --fusion \
-		$f_path $r_path -o $outdir/$base_sid --pseudobam --genomebam \
-		--gtf $ref_gtf -t 6 
-	fi
+		$f_path $r_path -o $outdir --pseudobam --genomebam \
+		--gtf $ref_gtf -t 14 
+	fi	
 fi
