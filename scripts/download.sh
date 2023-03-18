09efa405da1df2ee524bafbd499a291fe6365b55
nozerorma <n0zer0rma@gmail.com>
2023-03-18 10:13:57 +0100

    added more verbosity to fastqscreen genome dw

--- scripts/download.sh
+++ scripts/download.sh
@@ -29,7 +29,6 @@ while true; do
         exit 1
 		
     else
-		
         for SRAentry in "${SRAentries[@]}"; do
             if [[ "$SRAentry" =~ ^[Ss]RR[0-9]{6}$ ]]; then
                 echo "Matched SRA accession $SRAentry"
@@ -41,10 +40,12 @@ while true; do
 				echo -e "\nDumping fastq files from $SRAentry...\n"
 					if [ "$(ls -A $sample_dir/dumped_fastq/$SRAentry)" ]; then
 						echo -e "Fastq already dumped for $SRAentry, skipping.\n"
+					
 					else
 						(fasterq-dump -fp -O $sample_dir/dumped_fastq/$SRAentry $SRAentry) & spinner $!
 					fi				
-            else
+            
+			else
                 echo "Did not match SRA accession $SRAentry. Skipping download."
             fi
         done
